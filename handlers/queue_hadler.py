e81ebc6e35dd31082a088aa67470c57f85088a1f
4C01 <catcat-jia@foxmail.com>
2026-01-25 22:02:37 +0800

    fking str

--- handlers/queue_hadler.py
+++ handlers/queue_hadler.py
@@ -11,15 +11,15 @@ async def handle_join_command(websocket, message_text, user_id, group_id):
     if len(parts) == 2:
         queue_name = parts[1]
         if queue_name in ["0", "300", "600"]:
-            response_message = join_queue(user_id, queue_name)
+            response_message = join_queue(str(user_id), queue_name)
             await send_message(websocket, response_message, user_id, group_id)
             return
         else:
             await send_message(websocket, "queue种类需为: 0 300 600", user_id, group_id)
             return
 
-    queue_name = EloUtils.get_max_queue(PlayerUtils.get_elo(user_id))
-    join_queue(user_id, queue_name)
+    queue_name = EloUtils.get_max_queue(PlayerUtils.get_elo(str(user_id)))
+    join_queue(str(user_id), queue_name)
     return
 
 
@@ -35,7 +35,7 @@ async def handle_queue_stats_command(websocket, message_text, user_id, group_id)
         else:
             await send_message(websocket, "queue种类需为: 0 300 600", user_id, group_id)
             return
-    queue_name = get_user_queue(user_id)
+    queue_name = get_user_queue(str(user_id))
 
     if queue_name == "None":
         await send_message(websocket, "queue种类需为: 0 300 600", user_id, group_id)
@@ -46,7 +46,7 @@ async def handle_queue_stats_command(websocket, message_text, user_id, group_id)
     return
 
 async def handle_leave_queue(websocket, message_text, user_id, group_id):
-    await send_message(websocket, leave_queue(user_id), user_id, group_id)
+    await send_message(websocket, leave_queue(str(user_id)), user_id, group_id)
     return
 
 
@@ -72,5 +72,5 @@ async def handle_force_join_command(websocket, message_text, user_id, group_id):
         return
     else:
         queue_name = EloUtils.get_max_queue(PlayerUtils.get_elo(user_id))
-        join_queue(user_id, queue_name, True)
+        join_queue(str(user_id), queue_name, True)
         return
